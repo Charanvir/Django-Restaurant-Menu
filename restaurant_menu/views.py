65b62888b89124cf14dad5bfc5b134058444013c
Charanvir <charanvir123@gmail.com>
2023-06-08 18:59:35 -0400

    Added hard coded context data to index.html

--- restaurant_menu/views.py
+++ restaurant_menu/views.py
@@ -10,6 +10,13 @@ class MenuList(generic.ListView):
     queryset = Item.objects.order_by("-date_created")
     template_name = "index.html"
 
+    def get_context_data(self):
+        context = {
+            "meals": ["Pizza", "Burrito"],
+            "ingredients": ["Beans", "Cheese", "Green Peppers"]
+        }
+        return context
+
 
 # DetailView is better for individual item views
 class MenuItemDetail(generic.DetailView):
