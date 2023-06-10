9b1efd09e017093df4f36fbd6b99054132f6223f
Charanvir <charanvir123@gmail.com>
2023-06-10 13:45:41 -0400

    Added dynamic linking and added conditionals to html

--- restaurant_menu/urls.py
+++ restaurant_menu/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     # as_view renders the class as a view
     # Method is only needed in class based views
     path("", views.MenuList.as_view(), name="home"),
-
+    path("item/<int:pk>/", views.MenuItemDetail.as_view(), name="menu_item")
 ]
--- restaurant_menu/views.py
+++ restaurant_menu/views.py
@@ -10,13 +10,15 @@ class MenuList(generic.ListView):
     queryset = Item.objects.order_by("-date_created")
     template_name = "index.html"
 
-    def get_context_data(self):
-        context = {}
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
         context["meals"] = MEAL_TYPE
         return context
 
 
 # DetailView is better for individual item views
 class MenuItemDetail(generic.DetailView):
+    # We are able to access the item because we are creating the model variable
+    # and it being passed to the template
     model = Item
     template_name = "menu_item_detail.html"
